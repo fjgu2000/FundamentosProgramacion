87e992a3e824621dd8c7456b1c7078a85ef5dde0
fjgu2000 <128846050+fjgu2000@users.noreply.github.com>
2023-03-31 17:59:51 -0500

    Add files via upload
    Estos son procesos de ciclos anidados con funciones

---
+++ FactorialFun/FactorialFun/Program.cs
@@ -0,0 +1,29 @@
+﻿int n = 0, total = 0;
+Console.WriteLine("Digite por favor un número");
+n = int.Parse(Console.ReadLine());
+total=CicloExt(n);
+Console.WriteLine("La suma de los números factoriales es "+total);
+
+static int CicloExt(int n)
+{
+    int total = 0, fac = 0, num;
+    for(int i=1;i <= n;i++)
+    {
+        Console.WriteLine("Digite un número por favor");
+        num = int.Parse(Console.ReadLine());
+        fac = CicloInt(num);
+        Console.WriteLine("El valor factorial de " + num + " es " + fac);
+        total += fac;
+    }
+    return total;
+}
+
+static int CicloInt(int num)
+{
+    int fac = 1;
+    for(int j=2;j <= num;j++)
+    {
+        fac *= j;
+    }
+    return fac;
+}
---
+++ TablaMultiplicar/TablaMultiplicar/Program.cs
@@ -0,0 +1,24 @@
+﻿static void CicloInt(int i)
+{
+    int j = 1;
+    while (j <= 12)
+    {
+        Console.WriteLine(i + " X " + j + " = " + (i * j));
+        j++;
+    }
+    Console.WriteLine(" ");
+}
+static void CicloExt(int num)
+{
+    int i = 1;
+    while (i <= num)
+    {
+        CicloInt(i);
+        i++;
+    }
+}
+
+int num = 0;
+Console.WriteLine("Digite por favor un número");
+num = int.Parse(Console.ReadLine());
+CicloExt(num);
