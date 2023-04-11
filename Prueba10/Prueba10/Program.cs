121091b7e82af23cee6977df93fa6aec3b2ef8c3
fjgu2000 <128846050+fjgu2000@users.noreply.github.com>
2023-04-11 09:56:03 -0500

    Programa de notas
    Programa de notas para los estudiantes y la cantidad que ganaron

---
+++ Prueba10/Prueba10/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int n, ga = 0;
+Console.WriteLine("Digite por favor la cantidad de estudiantes");
+n = int.Parse(Console.ReadLine());
+ga = Ciclo(n);
+Console.WriteLine("La cantidad de estudiantes que ganaron es " + ga);
+
+static int Ciclo(int n)
+{
+    double n1, n2, n3, nf = 0;
+    int ga = 0;
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine("Digite por favor la nota 1");
+        n1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Digite por favor la nota 2");
+        n2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Digite por favor la nota 3");
+        n3 = double.Parse(Console.ReadLine());
+        nf = (n1 + n2 + n3) / 3;
+        ga += Condicion(nf);
+        Console.WriteLine("Su nota defenitiva es " + nf);
+    }
+    return ga;
+}
+
+static int Condicion(double nf)
+{
+   int ga= 0;
+    if (nf > 2.9)
+    {
+        ga++;
+    }
+    return ga;
+}
